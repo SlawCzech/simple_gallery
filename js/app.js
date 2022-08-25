b6cfd121c40ab64e6159293dcf93a10ef4615399
SlawCzech <slaw.czech@gmail.com>
2022-08-25 13:41:32 +0200

    done

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
-/*
- <div class="fullScreen">
-   <img src="./images/wrong.png">
-   <button class="close">Close</button>
- </div>
- */
+const picsRef = document.querySelectorAll(".gallery li");
+const bodyRef = document.querySelector("body");
+
+
+picsRef.forEach(item => {item.addEventListener("click", function() {
+        const thisPic = this.innerHTML;
+        const newDiv = document.createElement("div");
+        newDiv.classList.add("fullScreen");
+        newDiv.innerHTML = thisPic;
+        const closeButton = document.createElement("button");
+        closeButton.classList.add("close");
+        closeButton.innerText = "Close";
+        newDiv.appendChild(closeButton);
+        bodyRef.appendChild(newDiv);
+        closeButton.addEventListener("click", function() {
+            newDiv.remove();
+        })
+    }
+)});
